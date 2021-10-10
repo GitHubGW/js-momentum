2442461ccd56252f36a281773378c6a59fb57f6a
GW <kowonp@gmail.com>
2021-10-10 15:33:39 +0900

    Using LocalStorage

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
+const username = document.querySelector("#username");
 
 const handleSubmit = (event) => {
   event.preventDefault();
-};
-
-const handleLoginButton = (event) => {
-  const { value } = loginInput;
+  const inputValue = loginInput.value;
+  username.innerHTML = `Hello✋ ${inputValue}`;
+  localStorage.setItem("username", inputValue);
+  loginForm.classList.add("hidden");
+  username.classList.remove("hidden");
 };
 
 loginForm.addEventListener("submit", handleSubmit);
-loginButton.addEventListener("click", handleLoginButton);
