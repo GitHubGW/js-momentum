1ee5df68d2289a1173f65fe4582c8656491d737e
GW <kowonp@gmail.com>
2021-10-15 18:46:46 +0900

    Loading Username

--- js/app.js
+++ js/app.js
@@ -3,13 +3,22 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 const username = document.querySelector("#username");
 
+const HIDDEN = "hidden";
+const USERNAME = "username";
+const localStorageUsername = localStorage.getItem(USERNAME);
+
 const handleSubmit = (event) => {
   event.preventDefault();
-  const inputValue = loginInput.value;
-  username.innerHTML = `Hello✋ ${inputValue}`;
-  localStorage.setItem("username", inputValue);
-  loginForm.classList.add("hidden");
-  username.classList.remove("hidden");
+  username.innerHTML = `Hello✋ ${loginInput.value}`;
+  localStorage.setItem(USERNAME, loginInput.value);
+  loginForm.classList.add(HIDDEN);
+  username.classList.remove(HIDDEN);
 };
 
-loginForm.addEventListener("submit", handleSubmit);
+if (localStorageUsername === null) {
+  loginForm.classList.remove(HIDDEN);
+  loginForm.addEventListener("submit", handleSubmit);
+} else {
+  username.classList.remove(HIDDEN);
+  username.innerHTML = `Hello✋ ${localStorageUsername}`;
+}
