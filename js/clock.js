6a1c61a2c0fbbe6c4173c651aba0881011f9c597
GW <kowonp@gmail.com>
2021-10-23 16:19:20 +0900

    Added Clock

---
+++ js/clock.js
@@ -0,0 +1,17 @@
+const clock = document.querySelector("#clock");
+
+const handleClock = () => {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const parsedMinutes = String(minutes).padStart(2, "0");
+  const parsedSeconds = String(seconds).padStart(2, "0");
+
+  clock.innerHTML = `${hours}:${parsedMinutes}:${parsedSeconds}`;
+};
+
+if (clock) {
+  handleClock();
+  setInterval(handleClock, 1000);
+}
